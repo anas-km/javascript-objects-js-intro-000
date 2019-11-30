b1f1820927a0944ce9dbeca347d90ec530d77177
Anas K <mail.anascape@gmail.com>
2019-11-30 20:00:53 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,3 +1,18 @@
 var playlist =  {
-  bryan = 'Alison'
+  Slowdive: 'Alison',
+      ['My Bloody Valentine']: 'Sometimes'
+}
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist["Phil Ochs"] = "here is to the State of Mississippi"
+
+  return playlist
+}
+
+function  removeFromPlaylist(playlist, artistName, songTitle) {
+
+  delete playlist.Slowdive;
+
+  return playlist
+  
 }
